00677f9ac3b5e8280f7633c77f19d7026fd201b4
Jacob Park <jacobypark122@gmail.com>
2023-01-31 16:46:22 -0500

    played with scope

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Write your solution in this file!
+var customerName = "bob"
+
+function upperCaseCustomerName(name) {
+    customerName = customerName.toUpperCase()
+    return customerName
+}
+
+var bestCustomer
+function setBestCustomer() {
+    bestCustomer = "not bob"
+}
+
+function overwriteBestCustomer() {
+    bestCustomer = "maybe bob"
+}
+
+const leastFavoriteCustomer = 'sam'
+
+function changeLeastFavoriteCustomer() {
+    leastFavoriteCustomer = 'bob'
+}
